b3a42cf6a82365eaba19055443b54ca89279b077
Fredrik Ullman <fredrikullman93@gmail.com>
2020-10-07 19:54:02 +0200

    excercise

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import  React from "react";
+function App() {
+    return ( <div> 
+        <p> Wash dishes<input type="checkbox" /> </p>
+        <p>Clean <input type="checkbox" /> </p>
+         <p><input type="checkbox"/> Cook </p>
+             </div> )
+}
+
+export default App
