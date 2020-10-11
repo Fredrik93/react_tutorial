68224558feb565904f345f923858bd42ce250bd4
Fredrik Ullman <fredrikullman93@gmail.com>
2020-10-11 11:29:52 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import  React from "react";
 import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
 import About from './Components/About'
+import Nav from './Components/Nav'
 import Checkboxes from './Components/Checkboxes'
 function App() {
     return ( 
         <Router>
             <div> 
+                <Nav />
             <Switch>
             <Route path="/" exact component={Checkboxes} />
             <Route path="/about" component={About}/>
---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+function Nav(){
+return(
+    <nav>
+                    <h3>Logo</h3>
+        <ul className="nav-links">
+            <li>Checkboxes </li>
+            <li>About</li>
+        </ul>
+    </nav>
+)
+
+
+}
+export default Nav
