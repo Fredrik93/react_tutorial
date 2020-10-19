de617341621b5bdcb778c1376100ea96bfb8fe56
Fredrik Ullman <fredrikullman93@gmail.com>
2020-10-19 19:37:28 +0200

    add objects through props

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
 function ContactCard(props) {
-    console.log(props)
     const style = {
         width: "50vh",
         paddingTop: "5vh",
         border: '1px solid gray'
     }
     return (<div>
-        <img style={style} src={props.imgUrl}></img>
-        <h2>{props.name}</h2>
-        <p> Phone: {props.phone}</p>
-        <p> Email: {props.email} </p>
-        <p> Race: {props.race}</p>
+
+        <img style={style} src={props.contact.imgUrl}></img>
+        <h2>{props.contact.name}</h2>
+        <p> Phone: {props.contact.phone}</p>
+        <p> Email: {props.contact.email} </p>
+        <p> Race: {props.contact.race}</p>
     </div>)
 }
 export default ContactCard
