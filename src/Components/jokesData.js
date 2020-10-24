68aa12ffa6f57819856d27a89bb40a708de3e24c
Fredrik Ullman <fredrikullman93@gmail.com>
2020-10-24 17:04:31 +0200

    add jokesdata

---
+++ src/Components/jokesData.js
@@ -0,0 +1,33 @@
+const jokesData = [
+    {
+        id: 1,
+        question: "What’s the best thing about Switzerland?",
+        punchline: "I don’t know, but the flag is a big plus. "
+    },
+    {
+        id: 2,
+        question: "How does a rabbi make coffee? ",
+        punchline: "Hebrews it!"
+    },
+    {
+        id: 3,
+        question: "Rest in peace boiling water. ",
+        punchline: "You will be mist! "
+    },
+    {
+        id: 4,
+        question: "Why don't scientists trust atoms?",
+        punchline: "Because they make up everything! "
+    },
+    {
+        id: 5,
+        question: "Two artists had an art contest. ",
+        punchline: "It ended in a draw!"
+    },
+    {
+        id: 6,
+        punchline:
+            "I ate a clock yesterday, it was very time-consuming."
+    }
+]
+export default jokesData
