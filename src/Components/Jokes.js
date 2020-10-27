f7e6310d990d123918a00f51f55163a638c96cd0
Fredrik Ullman <fredrikullman93@gmail.com>
2020-10-27 18:01:56 +0100

    add filter!

--- src/Components/Jokes.js
+++ src/Components/Jokes.js
@@ -3,9 +3,22 @@ import jokeData from './jokesData'
 import Joke from './Joke'
 function Jokes() {
 
+    const words = ["for", "hi", "ta", "mm", "yoo", "wait"]
+    words.forEach(function (word) {
+        console.log(`${word} hey words!`)
+    })
+    const filteredWords = words.filter(function (word) {
+
+        return word.length > 3
+
+
+    })
+    console.log(filteredWords + " filtering ")
     const jokesComponent = jokeData.map(function (joke) {
         return <Joke key={joke.id} question={joke.question} punchline={joke.punchline} />
     })
+
+
 
     return (
         <div> {jokesComponent} </div>
