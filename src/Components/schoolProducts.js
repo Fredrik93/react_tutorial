96ade948fdd181b234da7c9c4a8eb942e3aab3a6
Fredrik Ullman <fredrikullman93@gmail.com>
2020-10-28 17:01:47 +0100

    add productdata

---
+++ src/Components/schoolProducts.js
@@ -0,0 +1,35 @@
+const products = [
+    {
+        id: "1",
+        name: "Pencil",
+        price: 1,
+        description: "Good for writing"
+    }, {
+        id: "2",
+        name: "Ruler",
+        price: 12,
+        description: "Ruling stuff"
+    }, {
+        id: "3",
+        name: "Eraser",
+        price: 21,
+        description: "Good for erasing"
+    }, {
+        id: "4",
+        name: "Coffee",
+        price: 12,
+        description: "mm"
+    }, {
+        id: "5",
+        name: "Tea",
+        price: 1,
+        description: "is okay"
+    }, {
+        id: "6",
+        name: "Snacks",
+        price: 1,
+        description: "snackysnack"
+    },
+
+
+]
