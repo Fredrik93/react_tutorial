474fe9b560fc2f32e7c67d21ccee04e3b1df1c0f
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-03 13:46:10 +0100

    change map function to es6

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -2,9 +2,7 @@ import React from 'react'
 import Product from './Product'
 import schoolProducts from './schoolProducts'
 
-const productComponent = schoolProducts.map(function (item) {
-    return <Product key={item.id} name={item.name} />
-})
+const productComponent = schoolProducts.map(item => <Product key={item.id} name={item.name} />)
 
 function Products(item) {
     return (
