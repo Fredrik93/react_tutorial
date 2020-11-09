420fbc8c86b9c7bc434bbb267ed1d12dfa91788d
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-09 16:36:06 +0100

    add onclick function

--- src/Components/LoggedIn.js
+++ src/Components/LoggedIn.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 
+
+function clickingButton() {
+    alert("ive been clicked!")
+}
+
 class LoggedIn extends Component {
     constructor() {
         super()
@@ -22,7 +27,7 @@ class LoggedIn extends Component {
                 <img style={{ width: "25vh" }} src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80" />
                 <br />
                 <br />
-                <button className="btn-success" > Click me</button>
+                <button className="btn-success" onClick={() => { alert("using es6 to write awesome short functions cool") }} > Click me</button>
             </span> </div>)
     }
 }
