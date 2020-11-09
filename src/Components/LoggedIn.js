a648fa8437a7ee63bac34b40ed6b509212aee65b
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-09 16:32:46 +0100

    style button

--- src/Components/LoggedIn.js
+++ src/Components/LoggedIn.js
@@ -14,7 +14,16 @@ class LoggedIn extends Component {
         } else {
             wordDisplay = "out"
         }
-        return <div className="todo-list" > Youre currently logged {wordDisplay} </div>
+        return (<div>
+            <div className="todo-list" >
+                Youre currently logged {wordDisplay}
+            </div>
+            <span className="todo-list" >
+                <img style={{ width: "25vh" }} src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80" />
+                <br />
+                <br />
+                <button className="btn-success" > Click me</button>
+            </span> </div>)
     }
 }
 
