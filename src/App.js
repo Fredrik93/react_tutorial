7d065e26174e9fceae94b9e8daf16d8f27d4f4a0
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-10 07:52:09 +0100

    clean up App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import About from './Components/About'
-import Checkbox from "./Components/Checkboxes";
 import Nav from './Components/Nav'
-import Time from './Components/Time'
-import Todos from './Components/Todos/Todos'
-import Contacts from './Components/Contacts/Contacts'
-import Jokes from './Components/Jokes/Jokes'
-import Home from './Components/Home'
-import Products from './Components/Products/Products'
-import Cars from './Components/Cars/Cars'
-import Projects from './Components/Cards/Projects'
-import StatePractice from "./Components/StatePractice";
-import LoggedIn from "./Components/LoggedIn";
-import Counter from "./Components/Counter";
+import Routing from './Routing'
 
 class App extends React.Component {
     constructor(props) {
@@ -33,34 +20,8 @@ class App extends React.Component {
     render() {
         return (
             <Fragment>
-
-                <Router>
-                    <div >
-                        <Nav />
-                        <Switch >
-                            <>
-                                <div  >
-                                    <Route path="/" exact component={Home} />
-                                    <div >
-                                        <Route path="/projects" component={Projects} />
-                                        <Route path="/cars" component={Cars} />
-                                        <Route path="/products" component={Products} />
-                                        <Route path="/contacts" component={Contacts} />
-                                        <Route path="/todos" component={Todos} />
-                                        <Route path="/jokes" component={Jokes} />
-                                        <Route path="/checkboxes" component={Checkbox} />
-                                        <Route path="/about" component={About} />
-                                        <Route path="/time" component={Time} />
-                                        <Route path="/statePractice" component={StatePractice} />
-                                        <Route path="/loggedIn" component={LoggedIn} />
-                                        <Route path="/counter" component={Counter} />
-                                    </div>
-                                </div>
-                            </>
-                        </Switch>
-
-                    </div>
-                </Router>
+                <Nav />
+                <Routing />
                 <p className="App-intro"> {this.state.myAPIResponse} </p>
             </Fragment>
         )
---
+++ src/Routing.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import About from './Components/About'
+import Checkbox from "./Components/Checkboxes";
+import Time from './Components/Time'
+import Todos from './Components/Todos/Todos'
+import Contacts from './Components/Contacts/Contacts'
+import Jokes from './Components/Jokes/Jokes'
+import Home from './Components/Home'
+import Products from './Components/Products/Products'
+import Cars from './Components/Cars/Cars'
+import Projects from './Components/Cards/Projects'
+import StatePractice from "./Components/StatePractice";
+import LoggedIn from "./Components/LoggedIn";
+import Counter from "./Components/Counter";
+
+function Routing() {
+    return (
+        <Router>
+            <div >
+
+                <Switch >
+                    <>
+                        <div  >
+                            <Route path="/" exact component={Home} />
+                            <div >
+                                <Route path="/projects" component={Projects} />
+                                <Route path="/cars" component={Cars} />
+                                <Route path="/products" component={Products} />
+                                <Route path="/contacts" component={Contacts} />
+                                <Route path="/todos" component={Todos} />
+                                <Route path="/jokes" component={Jokes} />
+                                <Route path="/checkboxes" component={Checkbox} />
+                                <Route path="/about" component={About} />
+                                <Route path="/time" component={Time} />
+                                <Route path="/statePractice" component={StatePractice} />
+                                <Route path="/loggedIn" component={LoggedIn} />
+                                <Route path="/counter" component={Counter} />
+                            </div>
+                        </div>
+                    </>
+                </Switch>
+
+            </div>
+        </Router>
+    )
+}
+export default Routing
