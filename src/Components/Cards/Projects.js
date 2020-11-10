ec3b56794ff92f0c54be31e2ee18d3930a2023f6
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-10 13:44:19 +0100

    update jomiola agnart card

--- src/Components/Cards/Projects.js
+++ src/Components/Cards/Projects.js
@@ -3,35 +3,46 @@ import Projects from './Carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Card, Button, Container, Row } from 'react-bootstrap';
 import uicScreenshot from '../../Assets/screenshot-homepage.png'
+import agnartScreenShot from '../../Assets/agnart-screenshot.png'
+import jomiolaScreenShot from '../../Assets/jomiola-screenshot.png'
+
 function List() {
 
     return (
         <Container  >
             <Row>
-                <Card style={{ width: '18rem', marginTop: "10vh", marginLeft: "13vh" }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                <Card style={{ width: '18rem', marginTop: "10vh", marginLeft: "9vh" }}>
+                    <a href="https://ullmanitconsulting.se/">
+                        <Card.Img variant="top" src={agnartScreenShot} />
+                    </a>
                     <Card.Body>
                         <Card.Title> agnart.se </Card.Title>
                         <Card.Text>
                             A website project with a client
                     </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <form action="https://agnart.se/">
+                            <Button type="submit" variant="primary">Check it Out</Button>
+                        </form>
                     </Card.Body>
 
                 </Card>
-                <Card style={{ width: '18rem', marginTop: "10vh", marginLeft: "1vh" }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                <Card style={{ width: '18rem', marginTop: "10vh", marginLeft: "5vh" }}>
+                    <a href="https://jomiola.se//" >
+                        <Card.Img variant="top" src={jomiolaScreenShot} />
+                    </a>
                     <Card.Body>
                         <Card.Title>Jomiola.se</Card.Title>
                         <Card.Text>
                             A website project with a client
 
                     </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <form action="https://ullmanitconsulting.se/">
+                            <Button type="submit" variant="primary">Check it Out</Button>
+                        </form>
                     </Card.Body>
 
                 </Card>
-                <Card style={{ width: '18rem', marginTop: "10vh", marginLeft: "1vh" }}>
+                <Card style={{ width: '18rem', marginTop: "10vh", marginLeft: "5vh" }}>
                     <a href="https://ullmanitconsulting.se/" >
                         <Card.Img variant="top" src={uicScreenshot} />
                     </a>
@@ -41,7 +52,7 @@ function List() {
                             The website for my business
                     </Card.Text>
                         <form action="https://ullmanitconsulting.se/">
-                            <Button type="submit" variant="primary">Go somewhere</Button>
+                            <Button type="submit" variant="primary">Check it Out</Button>
                         </form>
                     </Card.Body>
 
