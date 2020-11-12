723eeed2ced0cc39b16340ff2cec27eb1cb95927
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-12 07:26:42 +0100

    add states

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React from "react";
 import Nav from './Components/Nav'
 import Routing from './Routing'
 
-function count() {
-    function test() {
-        console.log("wow")
-    }
-    return (<div style={{ display: "block", textAlign: "center" }} > <h3> 0</h3>
-        <button className="btn btn-info" onClick={test}>Change number</button>
-    </div>)
-}
 function App() {
     return (
         <div>
             <Nav />
             <Routing />
-            {count()}
+
         </div>
     )
 }
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react'
 
-
 class Counter extends React.Component {
     constructor() {
         super()
         this.state = {
-            number: 0
+            number: 0,
+            otherNum: 0
         }
         this.handleCounter = this.handleCounter.bind(this)
+        this.handleCounterForOtherNum = this.handleCounterForOtherNum.bind(this)
     }
     handleCounter() {
         this.setState(previousState => {
@@ -17,17 +18,23 @@ class Counter extends React.Component {
         })
         console.log()
     }
+    handleCounterForOtherNum() {
+        this.setState(previousState => {
+            return {
+                otherNum: previousState.otherNum + 1
+            }
+        })
+    }
 
 
     render() {
-
-
         const stylingNumber = {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
             marginTop: "15vh",
-            fontSize: "20vh"
+            fontSize: "20vh",
+
 
         }
         const stylingButton = {
@@ -35,20 +42,25 @@ class Counter extends React.Component {
             alignItems: 'center',
             justifyContent: 'center',
             marginTop: "3vh"
-
         }
 
 
         return (
             <Fragment>
                 <div style={stylingNumber} >
-                    <span>{this.state.number}</span>
+                    <span style={{ padding: "5vh" }} >{this.state.otherNum}
+                        <div style={stylingButton}>
+                            <button className="btn-lg btn-info" onClick={this.handleCounterForOtherNum} > Change ! </button>
+                        </div>
+                    </span>
+                    <span>{this.state.number}
+                        <div style={stylingButton}>
+                            {/* when the method is part of the class we need the 'this.' */}
+                            <button className="btn-lg btn-success" onClick={this.handleCounter} >Change!</button>
+                        </div>
+                    </span>
                 </div>
 
-                <div style={stylingButton}>
-                    {/* when the method is part of the class we need the 'this.' */}
-                    <button className="btn-lg btn-success" onClick={this.handleCounter} >Change!</button>
-                </div>
             </Fragment>
         )
     }
