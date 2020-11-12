a9ec6ce16c08be92cb56472fdb5bc03f7705a41d
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-12 07:34:54 +0100

    add logic to counter

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -4,11 +4,10 @@ class Counter extends React.Component {
     constructor() {
         super()
         this.state = {
-            number: 0,
-            otherNum: 0
+            number: 0
         }
         this.handleCounter = this.handleCounter.bind(this)
-        this.handleCounterForOtherNum = this.handleCounterForOtherNum.bind(this)
+        this.decrementSum = this.decrementSum.bind(this)
     }
     handleCounter() {
         this.setState(previousState => {
@@ -18,11 +17,15 @@ class Counter extends React.Component {
         })
         console.log()
     }
-    handleCounterForOtherNum() {
+    decrementSum() {
+
         this.setState(previousState => {
-            return {
-                otherNum: previousState.otherNum + 1
+            if (this.state.number > 0) {
+                return {
+                    number: previousState.number - 1
+                }
             }
+
         })
     }
 
@@ -41,22 +44,24 @@ class Counter extends React.Component {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-            marginTop: "3vh"
+            marginTop: "3vh",
+
         }
 
 
         return (
             <Fragment>
                 <div style={stylingNumber} >
-                    <span style={{ padding: "5vh" }} >{this.state.otherNum}
-                        <div style={stylingButton}>
-                            <button className="btn-lg btn-info" onClick={this.handleCounterForOtherNum} > Change ! </button>
-                        </div>
-                    </span>
+
                     <span>{this.state.number}
+
                         <div style={stylingButton}>
                             {/* when the method is part of the class we need the 'this.' */}
-                            <button className="btn-lg btn-success" onClick={this.handleCounter} >Change!</button>
+                            <button className="btn-lg btn-success" onClick={this.handleCounter} >Add</button>
+                        </div>
+
+                        <div style={stylingButton}>
+                            <button className="btn-lg btn-info" onClick={this.decrementSum} > Remove </button>
                         </div>
                     </span>
                 </div>
