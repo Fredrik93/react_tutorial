55a3a337f15a34396242110c3ffcb60d41f93fa6
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-13 15:18:41 +0100

    fix bug state

--- src/Components/Todos/TodoItem.js
+++ src/Components/Todos/TodoItem.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 function TodoItem(props) {
     return (<h3 className="todo-item">
-        <input type="checkbox" checked={props.choreItem.completed}
-            onChange={() => { props.handleChange(props.choreItem.id) }} />
+        <input type="checkbox" checked={props.choreItem.completed} onChange={() => { props.handleChange(props.choreItem.id) }} />
         {props.choreItem.chore}  </h3>)
 } export default TodoItem
