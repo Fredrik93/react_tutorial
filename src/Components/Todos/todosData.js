101e8127fd595cdcbcafd407db4017b71efb7570
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-14 05:58:24 +0100

    todoslist

--- src/Components/Todos/todosData.js
+++ src/Components/Todos/todosData.js
@@ -2,12 +2,12 @@ const todosData = [
     {
         id: 1,
         chore: "Take out trash",
-        completed: false
+
     },
     {
         id: 2,
         chore: "Clean room",
-        completed: true
+
     }, {
         id: 3,
         chore: "Vacuum",
