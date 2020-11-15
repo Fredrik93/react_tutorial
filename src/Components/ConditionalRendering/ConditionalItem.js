7559fd554c0d7b9d1738a903c94b8d7f7deda5cf
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-15 11:02:24 +0100

    clean up ternary statement

--- src/Components/ConditionalRendering/ConditionalItem.js
+++ src/Components/ConditionalRendering/ConditionalItem.js
@@ -3,9 +3,8 @@ import React from 'react'
 function ConditionalItem(props) {
     return (<div>
         <h1>Nav</h1>
-        {props.isLoading === true ?
-            <h1> Loading... </h1>
-            : <h3>Something conditional </h3>}
+        {props.isLoading ? <h1> Loading... </h1> : <h3>Something conditional </h3>}
+        <h3> Footer</h3>
     </div>)
 }
 export default ConditionalItem
