2bb4f755304b0a3abbfa87f59e11f4a6a966b5c2
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-17 14:59:05 +0100

    state

--- src/Components/ApiData.js
+++ src/Components/ApiData.js
@@ -7,34 +7,37 @@ class ApiData extends React.Component {
         this.state = {
             myApiResponse: "",
             character: {},
-            loading: false
+            loading: false,
+            loadMyApi: false
         }
 
     }
     testCallApi() {
         fetch('http://localhost:3001/testAPI')
             .then(res => res.text())
-            .then(res => this.setState({ myApiResponse: res }))
+            .then(res => this.setState({ myApiResponse: res, loadMyApi: false }))
     }
 
     callStartWarsApi() {
         fetch('https://swapi.dev/api/people/1')
             .then(response => response.json())
-            .then(res => this.setState({ character: res }))
+            .then(res => this.setState({ character: res, loading: false }))
     }
     componentDidMount() {
         this.setState({ loading: true })
+        this.setState({ loadMyApi: true })
         this.callStartWarsApi()
         this.testCallApi()
     }
 
     render() {
-        let loadBeforeDisplay = this.state.loading ? <p> {this.state.character.name} </p> : <p> Loading ... </p>
+        let loadBeforeDisplay = this.state.loading ? "Loading ..." : this.state.character.name
+        let loadMyApi = this.state.loadMyApi ? "Loading ..." : this.state.myApiResponse
 
         return (
             <Container>
-                <p className="centerItem" style={{ border: "1px solid gray" }} > {this.state.myApiResponse} </p>
-                <div className="centerItem" style={{ border: "1px solid gray" }} > {loadBeforeDisplay} </div>
+                <p className="centerItem" style={{ border: "1px solid gray" }} > {loadMyApi} </p>
+                <p className="centerItem" style={{ border: "1px solid gray" }} > {loadBeforeDisplay} </p>
 
             </Container>
         )
