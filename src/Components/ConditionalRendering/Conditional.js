9b6e3424677e1f46d19d8e3916cf19c2afbf1026
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-17 10:30:21 +0100

    small clean up

--- src/Components/ConditionalRendering/Conditional.js
+++ src/Components/ConditionalRendering/Conditional.js
@@ -15,8 +15,8 @@ class Conditional extends React.Component {
     handleClick() {
         this.setState((prevState) => {
             prevState.isLoggedIn = !prevState.isLoggedIn
-            console.log("prevState logged in: ", prevState.isLoggedIn, " \n this.state.isLoading:", this.state.isLoading)
-            return { LoggedIn: prevState.isLoggedIn }
+            console.log("prevState logged in: ", prevState.isLoggedIn, " \n this.state.isLogged:", this.state.isLoggedIn)
+            return { thisObjectCanBeCalledWhatever: prevState.isLoggedIn }
 
         })
     }
@@ -43,13 +43,15 @@ class Conditional extends React.Component {
         }, 3500)
     }
     render() {
+        let buttonText = this.state.isLoggedIn ? <p>Log out</p> : <p>Log in</p>
+        let displayText = this.state.isLoggedIn ? <h3>Logged In</h3> : <h3>Logged Out </h3>
         return (
-
             <div style={{ textAlign: "center", marginTop: "10vh" }}>
                 { this.state.isLoading === true ? <h3> Loading...</h3> : <ConditionalItem />}
                 <div style={{ textAlign: "center", marginTop: "20vh" }}>
-                    {this.state.isLoggedIn ? <h3>Logged In</h3> : <h3>Logged Out </h3>}
-                    <button onClick={this.handleClick} style={{ height: "35px" }} className="btn btn-info" >{this.state.isLoggedIn ? <p>Log out</p> : <p>Log in</p>}</button>
+                    {displayText}
+                    <button onClick={this.handleClick}
+                        style={{ height: "35px" }} className="btn btn-info" >{buttonText}</button>
                 </div>
             </div >
         )
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import Conditionals from './ConditionalRendering/Conditional'
 class Counter extends React.Component {
     constructor() {
         super()
