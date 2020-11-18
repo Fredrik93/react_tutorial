88751e5a23e795a87553984998ad1ecec3a46a9c
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-18 06:40:25 +0100

    add state to form component

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
-function Forms() {
-    return (
-        <div>
-            <h3> Forms component</h3>
-        </div>
-    )
+class Forms extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            counter: 0
+        }
+        this.handleCounter = this.handleCounter.bind(this)
+    }
+    handleCounter() {
+        this.setState((prevState) => {
+            return { counter: prevState.counter + 1 }
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h3> {this.state.counter} </h3>
+                <button onClick={this.handleCounter} >Click </button>
+            </div>
+        )
+    }
 }
 export default Forms
