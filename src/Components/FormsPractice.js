2ad1e619d1dd94f7a26db20fed50d4600a0c2cf6
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-20 06:25:33 +0100

    add state to form

--- src/Components/FormsPractice.js
+++ src/Components/FormsPractice.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 class FormsPractice extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            age: 0
+        }
+    }
+    handleChange() {
+        console.log("lal")
+    }
     render() {
+
         return (
-            <h3>formspractice</h3>
+            <main>
+                <form>
+                    <input placeholder="First Name" onChange={this.handleChange} /> name:{this.state.firstName} < br />
+                    <input placeholder="Last Name" onChange={this.handleChange} /> < br />
+                    <input placeholder="Age" onChange={this.handleChange} /> < br />
+                    < br />
+                </form>
+            </main>
         )
     }
 } export default FormsPractice
