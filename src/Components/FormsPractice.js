48cdfadece1338e838a03b0673f6dd3b6088d112
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-22 09:06:46 +0100

    handle change on radiobtn

--- src/Components/FormsPractice.js
+++ src/Components/FormsPractice.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ThemeProvider } from 'react-bootstrap'
 
 class FormsPractice extends React.Component {
     constructor() {
@@ -6,7 +7,8 @@ class FormsPractice extends React.Component {
         this.state = {
             firstName: '',
             lastName: '',
-            age: ''
+            age: '',
+            gender: ''
         }
         this.handleChange = this.handleChange.bind(this)
 
@@ -14,10 +16,7 @@ class FormsPractice extends React.Component {
 
     handleChange(event) {
         const { name, value } = event.target
-        this.setState({
-            [name]: value
-        })
-
+        this.setState({ [name]: value })
     }
 
     render() {
@@ -28,6 +27,22 @@ class FormsPractice extends React.Component {
                     <input placeholder="First Name" name="firstName" onChange={this.handleChange} /> < br />
                     <input placeholder="Last Name" name="lastName" onChange={this.handleChange} />< br />
                     <input placeholder="Age" type="number" name="age" onChange={this.handleChange} />< br />
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            onChange={this.handleChange}
+                        /> Male</label>
+                    < br />
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            onChange={this.handleChange}
+                        /> Female</label>
+
                     < br />
                 </form>
 
@@ -36,8 +51,7 @@ class FormsPractice extends React.Component {
                 <div>
                     <h3>Your name : {this.state.firstName} {this.state.lastName}</h3>
                     <h3>Your age : {this.state.age}</h3>
-                    <label>  <input type="radio" name="gender" value="male" /> Male</label> < br />
-                    <label>  <input type="radio" name="gender" value="female" /> Female</label>
+                    <h3>Gender: {this.state.gender}</h3>
                 </div>
                 <br /> <button>Submit</button>
 
