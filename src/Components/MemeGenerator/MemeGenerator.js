2f27a42c416079b07ff0a96c4b7cf76f4319ac30
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-24 14:40:30 +0100

    add btn

--- src/Components/MemeGenerator/MemeGenerator.js
+++ src/Components/MemeGenerator/MemeGenerator.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
-function MemeGenerator() {
-    return <h1>MemeGenerator </h1>
+class MemeGenerator extends React.Component {
+
+    callMyApi() {
+        console.log("lalal")
+        fetch("http://localhost:3001/testAPI")
+            .then(response => response.text())
+            .then(response => this.setState({ apiResponse: response }))
+
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+
+        return (
+            <>
+                <h1>my Component has Mounted, Check the browser 'console'</h1>
+                <button>Click </button>
+            </>)
+    }
 }
 export default MemeGenerator
