7e3f653500aa8b3aa5f10d1a8e2256aa7e48606d
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-25 07:41:45 +0100

    initialize functioncall to api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./Components/Header";
 import Nav from './Components/Nav'
 import Routing from './Routing'
 
@@ -7,7 +6,6 @@ function App() {
     return (
         <div>
             <Nav />
-            <Header />
             <Routing />
 
         </div>
--- src/Components/MemeGenerator/MemeGenerator.js
+++ src/Components/MemeGenerator/MemeGenerator.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import Header from '../Header'
 import '../memeStyles.css'
 class MemeGenerator extends Component {
     constructor() {
         super()
         this.state = {
-            apiResponse: ""
+            apiResponse: '',
+            topText: '',
+            bottomText: '',
+            randomImage: 'https://i.imgflip.com/4nlown.jpg'
         }
         this.callMyApi = this.callMyApi.bind(this)
+        this.callToMemeAPI = this.callToMemeAPI.bind(this)
     }
     callMyApi() {
         console.log("lalal")
@@ -15,10 +20,21 @@ class MemeGenerator extends Component {
             .then(response => this.setState({ apiResponse: response }))
     }
 
+    callToMemeAPI() {
+        console.log("memeapi call")
+    }
+
     render() {
 
         return (
             <>
+                <Header />
+                <p>{this.callToMemeAPI()}</p>
+                <img src={this.state.randomImage} alt="meme" />
+                <br />
+                <br />
+                <br />
+                <br />
                 <p className="meme" >show api call by clicking button : <br /> {this.state.apiResponse}</p>
                 <button className="btn btn-success" onClick={this.callMyApi} >Click </button>
             </>
