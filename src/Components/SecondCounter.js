5c6420280aaff7e7c269fa833323ec4fc6e695ad
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-27 07:55:21 +0100

    add bind

--- src/Components/SecondCounter.js
+++ src/Components/SecondCounter.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 class SecondCounter extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            counter: 0
+        }
+        this.clickBtn = this.clickBtn.bind(this)
+    }
+    clickBtn() {
+        console.log("clicked")
+    }
     render() {
         return (
-            <div>
-                <h3> counter ex</h3>
+            <div style={{ textAlign: "center", marginTop: "10vh" }} >
+                <h1 style={{ fontSize: "20vh", border: "2px solid black" }} > 0 </h1>
+                <button className="btn btn-info" onClick={this.clickBtn} >Click me! </button>
             </div>
         )
     }
