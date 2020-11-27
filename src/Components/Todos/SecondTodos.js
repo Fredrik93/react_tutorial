b4770b5496e05f82191feb5bc2a1eaa44a04e498
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-27 15:45:00 +0100

    display todos from todosdata

--- src/Components/Todos/SecondTodos.js
+++ src/Components/Todos/SecondTodos.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Item from './SecondTodoItem'
+import todosData from './todosData'
 class SecondTodos extends React.Component {
+
+    showChores = todosData.map(Item => {
+        return Item.chore
+    })
+
     render() {
+        const style = {
+
+        }
         return (
             <div>
-                <Item chore="clean" />
+                {this.showChores}
             </div>
         )
     }
