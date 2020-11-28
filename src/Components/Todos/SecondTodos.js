64bd3b054d3ab6cf6dd1209234a1dd0901e6a017
Fredrik Ullman <fredrikullman93@gmail.com>
2020-11-28 15:20:05 +0100

    adding to todos

--- src/Components/Todos/SecondTodos.js
+++ src/Components/Todos/SecondTodos.js
@@ -2,10 +2,21 @@ import React from 'react'
 import Item from './SecondTodoItem'
 import todosData from './todosData'
 class SecondTodos extends React.Component {
-
+    constructor() {
+        super()
+        this.state = {
+            completedChores: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange() {
+        this.setState((prev) => {
+            console.log(prev)
+        })
+    }
 
     showChores = todosData.map(dataItem => {
-        return <Item chore={dataItem.chore} completo={true} key={dataItem.id} />
+        return <Item chore={dataItem.chore} completo={this.handleChange} key={dataItem.id} />
     })
 
     render() {
